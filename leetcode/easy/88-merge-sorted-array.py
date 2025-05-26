05c6d50947087bfac574264cd89ed86bc65feeee
Md Asraful <md.asraful@sicunet.net>
2025-05-26 17:44:47 +0600

    Implement Leetcode 88: Merge Sorted Array using optimal reverse two-pointer approach

--- leetcode/easy/88-merge-sorted-array.py
+++ leetcode/easy/88-merge-sorted-array.py
@@ -2,8 +2,8 @@
 # https://leetcode.com/problems/merge-sorted-array/
 
 """
-Approach: Two-pointer from the back
-Time Complexity: O(m * n)
+Approach: Merge Sorted Array using optimal reverse two-pointer approach
+Time Complexity: O(m +1 n)
 Space Complexity: O(1)
 """
 from typing import List
@@ -14,18 +14,21 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         total_length = m+n
-        num1_left_index, num2_left_index = 0, 0
-        while(num1_left_index < total_length and num2_left_index < n):
-            if nums1[num1_left_index] <= nums2[num2_left_index]:
-                num1_left_index+=1
+        num1_right_index, num2_right_index = m-1, n-1
+        insert_index = total_length-1
+        while(num1_right_index >= 0 and num1_right_index < total_length and num2_right_index >= 0 and num2_right_index < total_length ):
+            if nums1[num1_right_index] < nums2[num2_right_index]:
+                nums1[insert_index] = nums2[num2_right_index]
+                insert_index-=1
+                num2_right_index-=1
             else:
-                nums1.insert(num1_left_index, nums2[num2_left_index])
-                nums1.pop()
-                num2_left_index+=1
-        #Track last index 
-        last_index = total_length-(n-num2_left_index)
-        for index, rest_element in enumerate(nums2[num2_left_index:], start=last_index):
+                nums1[insert_index] = nums1[num1_right_index]
+                nums1[num1_right_index] = 0
+                insert_index-=1
+                num1_right_index-=1
+        
+        for index, rest_element in enumerate(nums2[:num2_right_index+1], start=0):
             nums1[index] = rest_element
-        # print(nums1)
+
 # instance = Solution()
 # print(instance.merge([4,0,0,0,0,0],1,[1,2,3,5,6],5))
