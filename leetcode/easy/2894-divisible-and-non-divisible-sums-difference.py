9a8082ac8aeca6b3e77fc5b9dbf255afadff1e6a
Md Asraful <md.asraful@sicunet.net>
2025-05-27 11:14:14 +0600

    Implement Leetcode 2894: Divisible and Non-Divisible Sums Difference using O(1) arithmetic formula

--- leetcode/easy/2894-divisible-and-non-divisible-sums-difference.py
+++ leetcode/easy/2894-divisible-and-non-divisible-sums-difference.py
@@ -2,19 +2,21 @@
 # https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/
 
 """
-Approach: Loop from 1 to n, summing values based on whether each is divisible by m or not.
-Time Complexity: O(n)
+Approach: sum of natural number law n*(n+1) //2 
+Time Complexity: O(1)
 Space Complexity: O(1)
 """
 
 class Solution:
     def differenceOfSums(self, n: int, m: int) -> int:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        num1 = sum([element for element in range(1, n+1) if element % m != 0])
-        num2 = sum([element for element in range(1, n+1) if element % m == 0])
+        
+        if m == 1:
+            return 0 - (n*(n+1) //2)   # Special case
+        total_sum = n*(n+1) // 2
+        multiples_count = n // m  # number of multiples of m up to n
+        num2 = (multiples_count*(multiples_count+1) // 2)*m
+        num1 = total_sum - num2  # Not divisible sum 
         return num1-num2
 
-# instance = Solution()
-# print(instance.differenceOfSums(10, 3))
+instance = Solution()
+print(instance.differenceOfSums(15, 9))
